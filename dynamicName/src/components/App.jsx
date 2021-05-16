1adbdbf1043fa0f1a1824706619a61ecea7fa67c
sakshijain009 <62885857+sakshijain009@users.noreply.github.com>
2021-05-16 23:44:33 +0530

    Changing State React

---
+++ dynamicName/src/components/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function setDetails() {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]:value
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" onChange={setDetails} value={contact.fName} />
+        <input name="lName" placeholder="Last Name" onChange={setDetails} value={contact.lName}/>
+        <input name="email" placeholder="Email" onChange={setDetails} value={contact.email}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
